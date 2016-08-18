a4f864b2048bcbab1708a4182a18791933643ed8
Corey Shott <jcshott@gmail.com>
2016-08-17 17:13:27 -0700

    now can specify auto renew amount

--- medications/forms.py
+++ medications/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-CHOICES = (("approve", "approve renewal"), ("contact", "contact patient"))
+CHOICES = (("approve", "Auto-approve renewal"), ("contact", "Contact patient for appointment"))
 
 class RenewalForm(forms.Form):
     action = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
     med_id = forms.CharField(widget=forms.widgets.HiddenInput())
+    renew_amt = forms.CharField(widget=forms.widgets.HiddenInput(), initial=0)
